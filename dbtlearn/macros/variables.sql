a769ab42d2ba900716651c38d066d9bc9d493135
manoritesandeep <manoritesandeep@gmail.com>
2025-10-07 22:25:36 -0700

    89. Working with dbt Variables

--- dbtlearn/macros/variables.sql
+++ dbtlearn/macros/variables.sql
@@ -1,6 +1,11 @@
 {% macro learn_variables() %}
 
+    {# Jinja Variable #}
     {% set your_name_jinja = "Sandeep"%}
     {{ log("Hello " ~ your_name_jinja, info=True)}}
+
+    {# dbt Variable: used with --vars '{variable: value}' #}
+    {{ log("Hello dbt user: " ~ var("user_name") ~ "!", info=True) }}
+    {# dbtlearn % dbt run-operation learn_variables --vars '{user_name: SandeepSolanki}' #}
 
 {% endmacro %}
