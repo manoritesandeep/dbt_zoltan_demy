deb6aa62ff8e295943cfdb5426c70a423bd6f4a5
manoritesandeep <manoritesandeep@gmail.com>
2025-10-07 22:31:16 -0700

    Setting Default values

--- dbtlearn/macros/variables.sql
+++ dbtlearn/macros/variables.sql
@@ -6,6 +6,7 @@
 
     {# dbt Variable: used with --vars '{variable: value}' #}
     {{ log("Hello dbt user: " ~ var("user_name") ~ "!", info=True) }}
+    {# {{ log("Hello dbt user: " ~ var("user_name", "DEFAULT USER HERE") ~ "!", info=True) }} #}
     {# dbtlearn % dbt run-operation learn_variables --vars '{user_name: SandeepSolanki}' #}
 
 {% endmacro %}
